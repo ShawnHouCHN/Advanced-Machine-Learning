594ebf3bf0f5b4f571b93cf57f565e27007592da
Oskar Hint <oskar.hint@gmail.com>
2018-05-02 02:07:27 +0200

    Using old loss_gradient functions

--- LMNN.py
+++ LMNN.py
@@ -41,11 +41,12 @@ class LargeMarginNearestNeighbor(KNeighborsClassifier):
         self.eta_index = None
         self.eta = None
 
+
     def fit(self, X, y):
 
         self.X = X
         self.y = y
-              
+
         # number of observations
         self.m = self.X.shape[0]
 
@@ -53,48 +54,46 @@ class LargeMarginNearestNeighbor(KNeighborsClassifier):
         self.d = self.X.shape[1]
 
         # initial L - just identity
-        if self.L_init is None :
+        if self.L_init is None:
             self.L_init = np.eye(self.d)
-            self.L_init = np.reshape(self.L_init, (self.d**2,))
+            self.L_init = np.reshape(self.L_init, (self.d ** 2,))
 
         # initial distances
         initial_distance = euclidean_distances(self.X, squared=True)
 
         # find indexes of k closest same class points in initial space
         # now saved as sparse matrix as well
-        
+
         self.dif_class_matrix = np.zeros(shape=(self.m, self.m))
         self.eta_index = np.empty(shape=(self.m, self.k), dtype=int)
         eta_row = np.repeat(range(self.m), self.k)
         eta_col = []
-       
-        
-        
+
         for i in range(self.m):
             # take points that belong to different class than point i
             dif_class = y != y[i]
-            self.dif_class_matrix[i,dif_class] = 1
+            self.dif_class_matrix[i, dif_class] = 1
             # take distances from point i to all other points
             point_distances = initial_distance[i,]
             # assign large values to different class points - so they will never be closest ones
             point_distances[dif_class] = max(point_distances) * 10
             # remember indexes of k closest same class neighbours for point i
-            eta_col = np.append(eta_col,point_distances.argsort()[1:self.k + 1])
+            eta_col = np.append(eta_col, point_distances.argsort()[1:self.k + 1])
             self.eta_index[i,] = point_distances.argsort()[1:self.k + 1]
-        self.eta = csr_matrix((np.repeat(1, self.m*self.k), (eta_row, eta_col)), shape=(self.m, self.m))
-        
-        #res = minimize(fun=self.loss_gradient, x0=self.L_init, method=self.method,
+        self.eta = csr_matrix((np.repeat(1, self.m * self.k), (eta_row, eta_col)), shape=(self.m, self.m))
+
+        # res = minimize(fun=self.loss_gradient, x0=self.L_init, method=self.method,
         #               jac=self.loss_simple_jac, options=self.options)
-        
+
         L, loss, details = fmin_l_bfgs_b(func=self.loss_gradient, x0=self.L_init, bounds=None,
-                                                  m=100, pgtol=self.tol, maxfun=500*self.max_iter,
-                                                  maxiter=self.max_iter, disp=4, callback=None)
-        
+                                         m=100, pgtol=self.tol, maxfun=500 * self.max_iter,
+                                         maxiter=self.max_iter, disp=4, callback=None)
+
         L_optim = L.reshape((self.d, self.d))
 
-        #L_optim_flat = np.reshape(L_optim, (self.d**2, ))
+        # L_optim_flat = np.reshape(L_optim, (self.d**2, ))
 
-        self.L_= L_optim
+        self.L_ = L_optim
         # alternative PCA  # Why the PCA?
         # pca = PCA(n_components=d)
         # a = pca.fit(X)
@@ -104,10 +103,10 @@ class LargeMarginNearestNeighbor(KNeighborsClassifier):
 
         # print(self.loss_simple(L_optim_flat))
         # print(self.loss_simple_jac(L_optim_flat))
-        
+
         # Fit a simple nearest neighbor classifier with the learned metric
-        super(LargeMarginNearestNeighbor, self).fit(self.transform(), y)   
-        
+        super(LargeMarginNearestNeighbor, self).fit(self.transform(), y)
+
         return self
     
     def predict(self, X):
@@ -160,80 +159,7 @@ class LargeMarginNearestNeighbor(KNeighborsClassifier):
             X = check_array(X)
 
         return X.dot(self.L_.T)
-    
-    
-    ################################################# Xiaoshen Implemntation ############################################################
-    def loss_gradient(self, L_in):
 
-        # transform L from vector back to matrix
-        L = np.reshape(L_in,(self.d, self.d))
-        # calculate linear transformation X L'
-        X_transformed = np.dot(np.asmatrix(self.X), np.asmatrix(L).T)
-
-        # calculate pairwise distances between all samples in transformed X
-        distance_matrix = euclidean_distances(X_transformed, squared=True)
-
-        # LMNN loss function
-        omega_1 = self.omega
-        
-        # calculate jacobian elementwise
-        jac = np.zeros((self.d, self.d))
-
-        # PULL - step
-        neighbour_distance_matrix = self.eta.multiply(distance_matrix)
-        pull_sum = omega_1 * neighbour_distance_matrix.sum()
-
-        # auxillary distances to DIFFERENT CLASS points
-        distance_matrix_aux = np.multiply(distance_matrix,self.dif_class_matrix)
-        distance_matrix_aux[distance_matrix_aux == 0] = 10 * np.amax(distance_matrix_aux)
-
-        impostor_num = 0
-        i_indexes = []
-        j_indexes = []
-        impostor_indexes = []
-        for i in range(self.m):
-            for j in self.eta_index[i,]:    
-                reference_distance = distance_matrix[i, j] + 1  # distance_matrix[i,j] + 1
-                ##impostor_num += sum(distance_matrix_aux[i, ] <= reference_distance)  ##Valentine
-                ##impostors = [j for j, x in enumerate(distance_matrix_aux[i, ] <= reference_distance) if x]  ##Valentine
-                impostors, = np.where((distance_matrix_aux[i, ] - reference_distance)<= 0 )
-                impostor_num += np.size(impostors)
-                i_indexes = np.append(i_indexes, np.repeat(i, np.size(impostors)))
-                j_indexes = np.append(j_indexes, np.repeat(j, np.size(impostors)))
-                impostor_indexes = np.append(impostor_indexes, impostors)
-                
-                if j < self.k: 
-                    index_j = self.eta_index[i, j]
-                    reference_distance = distance_matrix[i,index_j] + 1
-                    # find possible impostors
-                    impostors, = np.where((distance_matrix_aux[i, ] - reference_distance)< 0 )
-                    if np.size(impostors) > 0:
-                        for imp in impostors:
-                            p1 = 2 * np.dot((X_transformed[i, ].T - X_transformed[index_j, ].T), np.reshape((self.X[i, ] - self.X[index_j, ]), (1, self.d)))
-                            p2 = 2 * np.dot((X_transformed[i, ].T - X_transformed[imp, ].T), np.reshape((self.X[i, ] - self.X[imp, ]), (1, self.d)))
-                            jac += (1 - omega_1) * (p1 - p2)
-                        jac += omega_1 * 2 * np.dot((X_transformed[i, ].T - X_transformed[index_j, ].T), np.reshape((self.X[i, ] - self.X[index_j, ]), (1, self.d)))
-                    # if there is NO impostors for given pair x_i and x_j
-                    else:
-                        jac += omega_1 * 2 * np.dot((X_transformed[i, ].T - X_transformed[index_j, ].T), np.reshape((self.X[i, ] - self.X[index_j, ]), (1, self.d)))
-
-        #jac = np.reshape(jac, (1, self.d**2))                
-                
-        i_indexes = i_indexes.astype(int)
-        j_indexes = j_indexes.astype(int)
-        impostor_indexes = impostor_indexes.astype(int)
-        push_sum = 0
-        for n in range(impostor_num):
-            i = i_indexes[n]
-            j = j_indexes[n]
-            l = impostor_indexes[n]
-            push_sum += (1 + distance_matrix[i, j] - distance_matrix[i, l])
-        push_sum = (1 - omega_1) * push_sum
-        total_loss = push_sum + pull_sum   
-        
-        
-        return total_loss, L.dot(jac).flatten()
-   ################################################################################################################################################
 
     def loss_simple(self, L_in):
 
@@ -330,5 +256,11 @@ class LargeMarginNearestNeighbor(KNeighborsClassifier):
                 else:
                     jac += omega_1 * 2 * np.dot((X_transformed[i, ].T - X_transformed[index_j, ].T), np.reshape((self.X[i, ] - self.X[index_j, ]), (1, self.d)))
 
-        jac = np.reshape(jac, (1, self.d**2))
+        #jac = np.reshape(jac, (1, self.d**2))
         return jac
+
+    def loss_gradient(self, L_in):
+        loss = self.loss_simple(L_in)
+        grad = self.loss_simple_jac(L_in)
+
+        return loss, grad.flatten()
--- LMNN_SS.py
+++ LMNN_SS.py
@@ -158,142 +158,6 @@ class SemiSupervisedLargeMarginNearestNeighbor(KNeighborsClassifier):
         super(SemiSupervisedLargeMarginNearestNeighbor, self).fit(self.transform(X_labeled), y)
 
         return self
-
-    ################################################################   Xiaoshen's Implementation #######################################
-    def loss_gradient(self, L_in):
-
-        # transform L from vector back to matrix
-        self.L = np.reshape(L_in, (self.d, self.d))
-
-        # calculate linear transformation X L'
-        X_transformed = np.dot(np.asmatrix(self.X_labeled), np.asmatrix(self.L).T)
-        X_transformed_ul = np.dot(np.asmatrix(self.X_unlabeled), np.asmatrix(self.L).T)
-
-        # calculate pairwise distances between all samples in transformed X
-        distance_matrix = euclidean_distances(X_transformed, squared=True)
-        distance_matrix_ul = euclidean_distances(X_transformed, X_transformed_ul, squared=True)
-
-        # LMNN loss function
-        # calculate jacobian elementwise
-        jac = np.zeros((self.d, self.d))
-        jac_ul = np.zeros((self.d, self.d))
-
-        # PULL step - labeled part
-        neighbour_distance_matrix = self.eta.multiply(distance_matrix)
-        pull_sum = self.omega0 * neighbour_distance_matrix.sum()
-
-        # PULL step - unlabeled part
-        neighbour_distance_matrix_ul = self.eta_ul.multiply(distance_matrix_ul)
-        pull_sum_ul = self.omega1 * neighbour_distance_matrix_ul.sum()
-
-        # auxillary distances to DIFFERENT CLASS points in labeled dataset
-        distance_matrix_aux = np.multiply(distance_matrix, self.dif_class_matrix)
-        distance_matrix_aux[distance_matrix_aux == 0] = 100 * np.amax(distance_matrix_aux)
-
-        
-        
-        # PUSH step - labeled part
-        impostor_num = 0
-        i_indexes = []
-        j_indexes = []
-        impostor_indexes = []
-        for i in range(self.m):
-            for j in self.eta_index[i,]:    
-                reference_distance = distance_matrix[i, j] + 1  # distance_matrix[i,j] + 1
-                ##impostor_num += sum(distance_matrix_aux[i, ] <= reference_distance)  ##Valentine
-                ##impostors = [j for j, x in enumerate(distance_matrix_aux[i, ] <= reference_distance) if x]  ##Valentine
-                impostors, = np.where((distance_matrix_aux[i, ] - reference_distance)<= 0 )
-                impostor_num += np.size(impostors)
-                i_indexes = np.append(i_indexes, np.repeat(i, np.size(impostors)))
-                j_indexes = np.append(j_indexes, np.repeat(j, np.size(impostors)))
-                impostor_indexes = np.append(impostor_indexes, impostors)
-                
-                if j < self.k: 
-                    index_j = self.eta_index[i, j]
-                    reference_distance = distance_matrix[i,index_j] + 1
-                    # find possible impostors
-                    impostors, = np.where((distance_matrix_aux[i, ] - reference_distance)< 0 )
-                    if np.size(impostors) > 0:
-                        for imp in impostors:
-                            p1 = 2 * np.dot((X_transformed[i, ].T - X_transformed[index_j, ].T), np.reshape((self.X_labeled[i, ] - self.X_labeled[index_j, ]), (1, self.d)))
-                            p2 = 2 * np.dot((X_transformed[i, ].T - X_transformed[imp, ].T), np.reshape((self.X_labeled[i, ] - self.X_labeled[imp, ]), (1, self.d)))
-                            jac += (1 - self.omega0) * (p1 - p2)
-                        jac += self.omega0 * 2 * np.dot((X_transformed[i, ].T - X_transformed[index_j, ].T), np.reshape((self.X_labeled[i, ] - self.X_labeled[index_j, ]), (1, self.d)))
-                # if there is NO impostors for given pair x_i and x_j
-                    else:
-                        jac += self.omega0 * 2 * np.dot((X_transformed[i, ].T - X_transformed[index_j, ].T), np.reshape((self.X_labeled[i, ] - self.X_labeled[index_j, ]), (1, self.d)))
-        #jac = np.reshape(jac, (1, self.d**2))                
-                
-        i_indexes = i_indexes.astype(int)
-        j_indexes = j_indexes.astype(int)
-        impostor_indexes = impostor_indexes.astype(int)
-        push_sum = 0
-        for n in range(impostor_num):
-            i = i_indexes[n]
-            j = j_indexes[n]
-            l = impostor_indexes[n]
-            push_sum += (1 + distance_matrix[i, j] - distance_matrix[i, l])
-        
-        push_sum = (1 - self.omega0) * push_sum
-        
-        
-        
-        #  PUSH step - unlabeled part
-        impostor_num_ul = 0
-        i_indexes_ul = []
-        j_indexes_ul = []
-        impostor_indexes_ul = []
-        for i in range(self.m):
-            for j in self.eta_index_ul[i, ]:
-                reference_distance = distance_matrix_ul[i, j] + 1
-                impostor_num_ul += sum(distance_matrix_aux[i, ] <= reference_distance)
-                impostors_ul = [j for j, x in enumerate(distance_matrix_aux[i, ] <= reference_distance) if x]
-                i_indexes_ul = np.append(i_indexes_ul, np.repeat(i, np.size(impostors_ul)))
-                j_indexes_ul = np.append(j_indexes_ul, np.repeat(j, np.size(impostors_ul)))
-                impostor_indexes_ul = np.append(impostor_indexes_ul, impostors_ul)
-                
-            if j < self.k:
-                # index of k-th nearest same class neighbour of i
-                index_j_ul = self.eta_index_ul[i, j]
-                # reference distance to deciding if there is a push-step derivative at all
-                reference_distance_ul = distance_matrix_ul[i, index_j_ul] + 1
-                # find possible impostors
-                impostors_ul = [z for z, x in enumerate(distance_matrix_aux[i, ] < reference_distance_ul) if x]
-                # if there are impostors for given pair x_i and x_j
-                if np.size(impostors_ul) > 0:
-                    for imp in impostors_ul:
-                        p1 = 2 * np.dot((X_transformed[i, ].T - X_transformed_ul[index_j_ul, ].T),
-                                         np.reshape((self.X_labeled[i, ] - self.X_unlabeled[index_j_ul, ]), (1, self.d)))
-                        p2 = 2 * np.dot((X_transformed[i, ].T - X_transformed[imp, ].T),
-                                         np.reshape((self.X_labeled[i, ] - self.X_labeled[imp, ]), (1, self.d)))
-                        jac_ul += (1 - self.omega1) * (p1 - p2)
-                    jac_ul += self.omega1 * 2 * np.dot((X_transformed[i, ].T - X_transformed_ul[index_j_ul, ].T),
-                                                    np.reshape((self.X_labeled[i, ] - self.X_unlabeled[index_j_ul, ]), (1, self.d)))
-                # if there is NO impostors for given pair x_i and x_j
-                else:
-                    jac_ul += self.omega1 * 2 * np.dot((X_transformed[i, ].T - X_transformed_ul[index_j_ul, ].T),
-                                                    np.reshape((self.X_labeled[i, ] - self.X_unlabeled[index_j_ul, ]), (1, self.d)))
-
-        jac_total = self.omega2 * jac + (1 - self.omega2) * jac_ul
-                
-
-        i_indexes_ul = i_indexes_ul.astype(int)
-        j_indexes_ul = j_indexes_ul.astype(int)
-        impostor_indexes_ul = impostor_indexes_ul.astype(int)
-
-        push_sum_ul = 0
-        for n in range(impostor_num_ul):
-            i_ul = i_indexes_ul[n]
-            j_ul = j_indexes_ul[n]
-            l_ul = impostor_indexes_ul[n]
-            push_sum_ul += (1 + distance_matrix_ul[i_ul, j_ul] - distance_matrix[i_ul, l_ul])
-
-        push_sum_ul = (1 - self.omega1) * push_sum_ul
-
-        total_loss = self.omega2 * (pull_sum + push_sum) + (1 - self.omega2) * (pull_sum_ul + push_sum_ul)
-
-        return total_loss, self.L.dot(jac_total).flatten()
-        
         
 
 
@@ -461,6 +325,96 @@ class SemiSupervisedLargeMarginNearestNeighbor(KNeighborsClassifier):
         #jac_total = np.reshape(jac_total, (1, self.d ** 2))
 
         return jac_total
+
+    def loss_gradient(self, L_in):
+
+        loss = self.loss_ss(L_in)
+        grad = self.loss_ss_jac(L_in)
+
+        return loss, grad.flatten()
+
+    def fit(self, X_labeled, y):
+
+        self.X_labeled = X_labeled
+
+        self.y = y
+
+        # number of observations in labeled set
+        self.m = self.X_labeled.shape[0]
+        # number of observations in unlabeled set
+        self.m_ul = self.X_unlabeled.shape[0]
+
+        # number of dimensions
+        self.d = self.X_labeled.shape[1]
+
+        # initial L - just identity
+        if self.L_init is None:
+            self.L_init = np.eye(self.d)
+
+        self.L_init = np.reshape(self.L_init, (self.d ** 2,))
+
+        # initial distances - labeled to labeled - m by m
+        initial_distance_ll = euclidean_distances(self.X_labeled, squared=True)
+
+        # initial distances - labeled to unlabeled - m by m_un
+        initial_distance_ul = euclidean_distances(self.X_labeled, self.X_unlabeled, squared=True)
+
+        # find indexes of k closest same class points in initial space
+        # now saved as sparse matrix as well
+        self.dif_class_matrix = np.zeros(shape=(self.m, self.m))
+        self.eta_index = np.empty(shape=(self.m, self.k), dtype=int)
+        eta_row = np.repeat(range(self.m), self.k)
+        eta_col = []
+
+        for i in range(self.m):
+            # take points that belong to different class than point i
+            dif_class = y != y[i]
+            self.dif_class_matrix[i, dif_class] = 1
+            # take distances from point i to all other points
+            point_distances = initial_distance_ll[i,]
+            # assign large values to different class points - so they will never be closest ones
+            point_distances[dif_class] = max(point_distances) * 10
+            # remember indexes of k closest same class neighbours for point i
+            eta_col = np.append(eta_col, point_distances.argsort()[0:self.k])
+            self.eta_index[i,] = point_distances.argsort()[0:self.k]
+
+        self.eta = csr_matrix((np.repeat(1, self.m * self.k), (eta_row, eta_col)), shape=(self.m, self.m))
+
+        # same procedure to find closest unlabeled points
+        self.eta_index_ul = np.empty(shape=(self.m, self.k), dtype=int)
+        eta_row_ul = np.repeat(range(self.m), self.k)
+        eta_col_ul = []
+
+        for i in range(self.m):
+            # take distances from point i to all other unlabeled points
+            point_distances = initial_distance_ul[i,]
+            # remember indexes of k closest unlabeled neighbours for point i
+            eta_col_ul = np.append(eta_col_ul, point_distances.argsort()[0:self.k])
+            self.eta_index_ul[i,] = point_distances.argsort()[0:self.k]
+
+        self.eta_ul = csr_matrix((np.repeat(1, self.m * self.k), (eta_row_ul, eta_col_ul)), shape=(self.m, self.m_ul))
+
+        # L = minimize(fun=self.loss_ss, x0=self.L_init, method=self.method, jac=self.loss_ss_jac,
+        #             options=self.options)
+
+
+        L, loss, details = fmin_l_bfgs_b(func=self.loss_gradient, x0=self.L_init.flatten(), bounds=None,
+                                         m=100, pgtol=self.tol, maxfun=500 * self.max_iter,
+                                         maxiter=self.max_iter, disp=4, callback=None)
+
+        # L, loss, details = fmin_l_bfgs_b(func=self.loss_gradient, x0=self.L_init.flatten(),
+        #                                                maxfun=50, maxiter=20, pgtol=0.001)
+
+        L_optim = np.reshape(L, [self.d, self.d])
+
+        self.L = L_optim
+        self.loss = loss
+        self.details = details
+
+        super(SemiSupervisedLargeMarginNearestNeighbor, self).fit(self.transform(X_labeled), y)
+
+        return self
+
 
     def score(self, X, y, sample_weight=None):
         """Returns the mean accuracy on the given test data and labels.
